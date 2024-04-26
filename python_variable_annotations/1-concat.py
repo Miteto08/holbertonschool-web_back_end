1887c00c9f7cd53cce916c29b554632076ccd74c
Miteto08 <julien.lechevalier@shortways.com>
2024-04-26 16:35:11 +0200

    task 1 - basic annotation concat

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+"""Basic annotations - concat
+mandatory"""
+
+
+def add(str1: str, str2: str) -> str:
+    """function that takes a string str1 and a string str2
+    as arguments and returns a concatened string."""
+    return (str1 + str2)
