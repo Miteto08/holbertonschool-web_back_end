dd563c36923a3db0ec19e2b68ccf34017b1ba970
Miteto08 <julien.lechevalier@shortways.com>
2024-04-26 16:47:14 +0200

    Task 2 - basic annotation floor

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+"""Basic annotations - floor
+mandatory"""
+
+
+def floor(n: float) -> float:
+    """function that takes takes a float n as argument
+    and returns the floor of the float."""
+    return (n)
