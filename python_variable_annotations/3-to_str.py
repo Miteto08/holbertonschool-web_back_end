35abe7c6574c998f48652fd5e4130c9f3efc151c
Miteto08 <julien.lechevalier@shortways.com>
2024-04-26 17:14:07 +0200

    task 3 - basic annotation - to string

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+"""Basic annotations - to string
+mandatory"""
+
+
+def to_str(n: float) -> str:
+    """function that takes a float n as argument
+    and returns the string representation of the float"""
+    return (str(n))
