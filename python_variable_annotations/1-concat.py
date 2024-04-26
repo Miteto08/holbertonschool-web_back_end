cbf9bc87b52b03ddf16a821816b4042543ea54bf
Miteto08 <julien.lechevalier@shortways.com>
2024-04-26 16:41:08 +0200

    task 1 - correction def name

--- python_variable_annotations/1-concat.py
+++ python_variable_annotations/1-concat.py
@@ -4,7 +4,7 @@
 mandatory"""
 
 
-def add(str1: str, str2: str) -> str:
+def concat(str1: str, str2: str) -> str:
     """function that takes a string str1 and a string str2
     as arguments and returns a concatened string."""
     return (str1 + str2)
