4c943b7d2301c1a3e43cc7cc4ddc58af06e61889
Miteto08 <julien.lechevalier@shortways.com>
2024-04-29 16:49:48 +0200

    Task5 - Try with input_list: List[float]

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -3,8 +3,10 @@
 """Complex types - list of floats
 mandatory"""
 
+from typing import List
 
-def sum_list(*input_list) -> float:
+
+def sum_list(*input_list: List[float]) -> float:
     """function that which takes a list input_list of floats as
     argument and returns their sum as a float."""
     add: float = 0
