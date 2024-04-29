020066b011c93c73df054f0b02dc6cb1e872f30c
Miteto08 <julien.lechevalier@shortways.com>
2024-04-29 21:40:17 +0200

    Task100 - Replace Optional to Union

--- python_variable_annotations/100-safe_first_element.py
+++ python_variable_annotations/100-safe_first_element.py
@@ -3,11 +3,11 @@
 """Duck typing - first element of a sequence
 mandatory"""
 
-from typing import Sequence, Any, Optional
+from typing import Sequence, Any, Union
 
 
 # The types of the elements of the input are not known
-def safe_first_element(lst: Sequence[Any]) -> Optional[Any]:
+def safe_first_element(lst: Sequence[Any]) -> Union[Any]:
     if lst:
         return lst[0]
     else:
