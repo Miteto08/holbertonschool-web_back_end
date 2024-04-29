c6a8841b52741d2ffa26b44e80dfa1e1a9ed6bf9
Miteto08 <julien.lechevalier@shortways.com>
2024-04-29 21:45:19 +0200

    blabla

--- python_variable_annotations/100-safe_first_element.py
+++ python_variable_annotations/100-safe_first_element.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python3
-
 """Duck typing - first element of a sequence
-mandatory"""
+"""
 
-from typing import Sequence, Any, Optional
+from typing import Any, Sequence, Union
 
 
-# The types of the elements of the input are not known
-def safe_first_element(lst: Sequence[Any]) -> Optional[Any]:
+# The types of the elements of the input are not know
+def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
     """Duck typing"""
     if lst:
         return lst[0]
     else:
         return None
+    
