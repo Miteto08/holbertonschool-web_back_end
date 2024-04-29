86be17b58a2bd618cc67384c80d48ba5eb639eac
Miteto08 <julien.lechevalier@shortways.com>
2024-04-29 16:48:43 +0200

    Task5 - Try with float as num and add: function

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -5,9 +5,9 @@ mandatory"""
 
 
 def sum_list(*input_list) -> float:
-    sum = 0
-    for num in input_list:
-        sum += num
     """function that which takes a list input_list of floats as
     argument and returns their sum as a float."""
-    return (sum(float(input_list)))
+    add: float = 0
+    for i in input_list:
+        add += i
+    return (add)
