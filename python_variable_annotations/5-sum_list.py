e0bbc97ad745815eb4649f70de595b00daca1cde
Miteto08 <julien.lechevalier@shortways.com>
2024-04-29 16:55:33 +0200

    Task5 - Try with just return

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -6,10 +6,7 @@ mandatory"""
 from typing import List
 
 
-def sum_list(*input_list: List[float]) -> float:
+def sum_list(input_list: List[float]) -> float:
     """function that which takes a list input_list of floats as
     argument and returns their sum as a float."""
-    add: float = 0
-    for i in input_list:
-        add += i
-    return (add)
+    return sum(input_list)
