5a5500a1d5f1d37c050057ed1f3e34e5f8fd0832
Miteto08 <julien.lechevalier@shortways.com>
2024-04-29 21:07:23 +0200

    forgot file

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -7,6 +7,8 @@ from typing import Union, Tuple
 
 
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
-    """function that which takes a list mxd_lst
-    of integers and floats and returns their sum as a float."""
+    """function that which takes a string k and an int OR float
+    v as arguments and returns a tuple. The first element of the
+    tuple is the string k. The second element is the square of the
+    int/float v and should be annotated as a float."""
     return (k, v * v)
