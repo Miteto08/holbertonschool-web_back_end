a4c16494b9d64e8235ea8ca507f0e5989df029a0
Julien <julien.lechevalier@shortways.com>
2024-07-10 10:52:14 +0200

    Task8 - Basic - Base64 decode

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,6 +2,9 @@
 """ Basic auth, Base64 part, Base64 decode, User credentials, User object,
     Overload current_user - and BOOM!, Allow password with ":" """
 from api.v1.auth.auth import Auth
+from base64 import b64decode
+from typing import TypeVar
+from models.user import User
 
 
 class BasicAuth(Auth):
@@ -19,3 +22,19 @@ class BasicAuth(Auth):
             return None
         base = authorization_header.split(' ')
         return base[1]
+    
+    def decode_base64_authorization_header(self,
+                                           base64_authorization_header: str
+                                           ) -> str:
+        """ returns the decoded value of a Base64 string """
+        if base64_authorization_header is None:
+            return None
+        if not isinstance(base64_authorization_header, str):
+            return None
+        try:
+            baseEncode = base64_authorization_header.encode('utf-8')
+            baseDecode = b64decode(baseEncode)
+            decodedValue = baseDecode.decode('utf-8')
+            return decodedValue
+        except Exception:
+            return None
