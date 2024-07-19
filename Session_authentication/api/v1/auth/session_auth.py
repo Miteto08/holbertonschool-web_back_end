b0bc3c52a807cbbccf8ccd72ab764b0784758c6d
Miteto08 <julien.lechevalier@shortways.com>
2024-07-19 10:22:55 +0200

    Correction indentation

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -16,13 +16,13 @@ class SessionAuth(Auth):
         session_id = str(uuid.uuid4())
         self.user_id_by_session_id[session_id] = user_id
         return session_id
-    
+
     def user_id_for_session_id(self, session_id: str = None) -> str:
         """ returns a User ID based on a Session ID """
         if session_id is None or not isinstance(session_id, str):
             return None
         return self.user_id_by_session_id.get(session_id)
-    
+
     def current_user(self, request=None):
         """ returns a User instance based on a cookie value """
         session_cookie = self.session_cookie(request)
@@ -30,7 +30,7 @@ class SessionAuth(Auth):
             return None
         _id = self.user_id_for_session_id(session_cookie)
         return User.get(_id)
-    
+
     def destroy_session(self, request=None):
         """Deletes de user session / logout"""
         if request is None:
