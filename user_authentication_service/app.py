fc24a7f0932bedf9bf462211c61e349d5e4b32c6
Miteto08 <julien.lechevalier@shortways.com>
2024-07-29 21:19:43 +0200

    error correction

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -128,7 +128,3 @@ def update_password() -> str:
 
     msg = {"email": email, "message": "Password updated"}
     return jsonify(msg), 200
-
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port="5000")
