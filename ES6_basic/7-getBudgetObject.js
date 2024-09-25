5a8f91a0bfdc7d560994bfa7ae3f6d6de4280127
Miteto08 <julien.lechevalier@shortways.com>
2024-09-25 19:54:31 +0200

    Task7 - Object property value shorthand syntax

---
+++ ES6_basic/7-getBudgetObject.js
@@ -0,0 +1,9 @@
+export default function getBudgetObject(income = income, gdp = gdp, capita = capita) {
+    const budget = {
+      income,
+      gdp,
+      capita,
+    };
+  
+    return budget;
+  }
