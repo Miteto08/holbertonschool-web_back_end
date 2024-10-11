19771b4d94f54a51aa4eb786de55698fd42af238
Miteto08 <julien.lechevalier@shortways.com>
2024-10-11 10:19:43 +0200

    Task0 - Keep every promise you make and only make promises you can keep

--- ES6_promise/0-promise.js
+++ ES6_promise/0-promise.js
@@ -1,5 +1,12 @@
-export function getResponseFromAPI() {
+export default function getFullResponseFromAPI(success) {
     return new Promise((resolve, reject) => {
-
-    })
-};
+      if (success === true) {
+        resolve({
+          status: 200,
+          body: 'Success',
+        });
+      } else {
+        reject(new Error('The fake API is not working currently'));
+      }
+    });
+  }
