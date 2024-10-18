c9a562ea76dae7a075bdd74eb1ad79ec6e65fe18
Miteto08 <julien.lechevalier@shortways.com>
2024-10-18 10:39:02 +0200

    Task9 - Throw error / try catch

---
+++ ES6_promise/9-try.js
@@ -0,0 +1,13 @@
+/* eslint-disable */
+export default function guardrail(mathFunction) {
+    const queue = [];
+    let value;
+    try {
+        value = mathFunction();
+    } catch (error) {
+        value = error.toString();
+    }
+    queue.push(value);
+    queue.push('Guardrail was processed');
+    return queue;
+}
