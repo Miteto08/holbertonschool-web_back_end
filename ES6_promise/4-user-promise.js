900abce901bdd07bafcee59226f930a405a72362
Miteto08 <julien.lechevalier@shortways.com>
2024-10-18 09:34:19 +0200

    Task4 - Simple promise

--- ES6_promise/4-user-promise.js
+++ ES6_promise/4-user-promise.js
@@ -1,5 +1,5 @@
 /* eslint-disable */
-function signUpUser(firstName, lastName) {
+export function signUpUser(firstName, lastName) {
     return Promise.resolve({
         firstName,
         lastName,
